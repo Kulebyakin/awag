19fb09d4ce99b791c0423817bea0058afbf1ab29
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-08-05 10:59:26 +0800

    Trying to validate source and destiny warehouses not to be equal

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -3,9 +3,9 @@ class Transfer < ApplicationRecord
   belongs_to :to_warehouse, class_name: 'Warehouse'
   belongs_to :product
 
-  accepts_nested_attributes_for :from_warehouse, :to_warehouse
-
   validates :from_warehouse, presence: true
   validates :to_warehouse, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  # validates_uniqueness_of :from_warehouse, scope: :to_warehouse
+  # validates_uniqueness_of :to_warehouse, scope: :from_warehouse
 end
--- app/models/transfer_start.rb
+++ app/models/transfer_start.rb
@@ -1,6 +1,21 @@
 class TransferStart
+  extend ActiveModel::Naming
   include ActiveModel::Model
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
   attr_accessor :from_warehouse, :to_warehouse
   validates :from_warehouse, presence: true
   validates :to_warehouse, presence: true
+  # validates :from_warehouse, uniqueness: { scope: :to_warehouse }
+  # validates :to_warehouse, uniqueness: { scope: :from_warehouse }
+  # validates_uniqueness_of :from_warehouse, scope: :to_warehouse
+  # validates_uniqueness_of :to_warehouse, scope: :from_warehouse
+  validate :to_warehouses_and_from_warehouse_must_be_different
+
+  def to_warehouses_and_from_warehouse_must_be_different
+    if :from_warehouse == :to_warehouse
+      errors.add(:transfer_start, "Выбирите разные склады")
+    end
+  end
 end
